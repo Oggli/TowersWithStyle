814643a65e3642534b99e46628368f3c0aa98f3c
Borusbulldog <borusbulldog@gmail.com>
2026-02-15 13:54:53 +0000

    Added basic gameplay mechanics
    Income, Credits, Killttracker

---
+++ Source/TowersWithStyle/PhysicsComponents/ProjectileOriginMesh.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "ProjectileOriginMesh.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERSWITHSTYLE_API AProjectileOriginMesh : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+};
